50b7876a0ffb3ba9493cfd19745fd59335bf73b4
Rzv888 <udinamin445@gmail.com>
2024-04-21 09:18:18 +0700

    Menghapus comment yang berlebihan-employee

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -46,11 +46,7 @@ public class Employee {
 		childNames = new LinkedList<String>();
 		childIdNumbers = new LinkedList<String>();
 	}
-	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
+
 	//Long Method Hasil Refactoring//
 	private int getBaseSalary(int grade) {
 		switch (grade) {
