80673e29ec246f02797d9a437f5e4622bf92526b
Rzv888 <udinamin445@gmail.com>
2024-04-21 10:18:58 +0700

    Memperbaiki logika fungsi

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -2,33 +2,37 @@ package lib;
 
 public class TaxFunction {
 
-	//Menghapus komen yang tidak perlu//
-	
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
-	}
-	
+ //Menghapus komen berlebihan//
+ 
+ //Memperbaiki logika fungsinya
+    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+        
+        if (numberOfMonthWorking > 12) {
+            System.err.println("More than 12 months working per year");
+            numberOfMonthWorking = 12; 
+        }
+        
+
+        if (numberOfChildren > 3) {
+            numberOfChildren = 3;
+        }
+        
+        int tax;
+        int totalIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
+        
+        if (isMarried) {
+            int childrenDeduction = numberOfChildren * 1500000;
+            int totalDeduction = deductible + 54000000 + 4500000 + childrenDeduction;
+            tax = (int) Math.round(0.05 * (totalIncome - totalDeduction));
+        } else {
+            int totalDeduction = deductible + 54000000;
+            tax = (int) Math.round(0.05 * (totalIncome - totalDeduction));
+        }
+
+        if (tax < 0) {
+            return 0;
+        } else {
+            return tax;
+        }
+    }
 }
