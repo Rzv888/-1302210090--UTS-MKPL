db6fabd728c6113c74eb242a54f98d206714d7f2
Rzv888 <udinamin445@gmail.com>
2024-04-21 09:52:34 +0700

    Mengubah format penulisan kode, Menhapus field yang tidak digunakan, mengubah yang awalnya private menjadi public

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -1,72 +1,65 @@
 package lib;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Employee {
 
-	private String employeeId;
-	private String firstName;
-	private String lastName;
-	private String idNumber;
-	private String address;
-	
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
-	private int monthWorkingInYear;
-	
-	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
-	private int monthlySalary;
-	private int otherMonthlyIncome;
-	private int annualDeductible;
-	
-	private String spouseName;
-	private String spouseIdNumber;
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String address;
 
-	private List<String> childNames;
-	private List<String> childIdNumbers;
-	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = idNumber;
-		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
-		this.isForeigner = isForeigner;
-		this.gender = gender;
-		
-		childNames = new LinkedList<String>();
-		childIdNumbers = new LinkedList<String>();
-	}
-	//Long Method Hasil Refactoring//
-	private int getBaseSalary(int grade) {
-		switch (grade) {
-			case 1:
-				return 3000000;
-			case 2:
-				return 5000000;
-			case 3:
-				return 7000000;
-			default:
-				throw new IllegalArgumentException("Invalid grade");
-		}
-	}
-	
-	private int calculateSalaryWithForeignerAdjustment(int baseSalary) {
-		return isForeigner ? (int) (baseSalary * 1.5) : baseSalary;
-	}
-	//Duplicate code hasil refactoring//
-	public void setMonthlySalary(int grade) {
-        int baseSalary = getBaseSalary(grade);
-        monthlySalary = calculateSalaryWithForeignerAdjustment(baseSalary);
+    private int yearJoined;
+    private int monthJoined;
+    private int dayJoined;
+    private int monthWorkingInYear;
+
+    private boolean isForeigner;
+    private boolean isMale;
+
+    private int monthlySalary;
+    private int otherMonthlyIncome;
+    private int annualDeductible;
+
+    private List<String> childNames;
+    private List<String> childIdNumbers;
+
+    public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean isMale) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.address = address;
+        this.yearJoined = yearJoined;
+        this.monthJoined = monthJoined;
+        this.dayJoined = dayJoined;
+        this.isForeigner = isForeigner;
+        this.isMale = isMale;
+
+        childNames = new LinkedList<>();
+        childIdNumbers = new LinkedList<>();
+    }
+
+    public void setMonthlySalary(int grade) {
+        int baseSalary;
+        switch (grade) {
+            case 1:
+                baseSalary = 3000000;
+                break;
+            case 2:
+                baseSalary = 5000000;
+                break;
+            case 3:
+                baseSalary = 7000000;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid grade");
+        }
+
+        monthlySalary = isForeigner ? (int) (baseSalary * 1.5) : baseSalary;
     }
 
     public void setAnnualDeductible(int deductible) {
@@ -77,23 +70,16 @@ public class Employee {
         this.otherMonthlyIncome = income;
     }
 
-    public void setSpouse(String spouseName, String spouseIdNumber) {
-        this.spouseName = spouseName;
-        this.spouseIdNumber = idNumber;
-    }
-
     public void addChild(String childName, String childIdNumber) {
         childNames.add(childName);
         childIdNumbers.add(childIdNumber);
     }
 
     public int getAnnualIncomeTax() {
-        int monthWorkingInYear = calculateMonthWorkingInYear();
-        return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
-    }
-
-    private int calculateMonthWorkingInYear() {
         LocalDate date = LocalDate.now();
-        return (date.getYear() == yearJoined) ? date.getMonthValue() - monthJoined : 12;
+
+        monthWorkingInYear = date.getYear() == yearJoined ? date.getMonthValue() - monthJoined : 12;
+
+        return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber == null || spouseIdNumber.isEmpty(), childIdNumbers.size());
     }
 }
